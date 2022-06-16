7fb718117434c71adb8958d13d2c92dccaa32a8d
pavel kocnar <66769431+PavelKosnar@users.noreply.github.com>
2022-06-16 12:30:04 +0200

    Add files via upload

--- shakes.py
+++ shakes.py
@@ -11,8 +11,13 @@ class Class:
     }
     
 class YourStats:
-    hp = 0
-    dmg = 0
+    hp = 15
+    dmg = 5
+	
+class EnemyStats:
+	hp = 20
+	dmg = 10
+
 
 def selectedClass(answer):
     for i in Class.allClasses:
@@ -20,17 +25,32 @@ def selectedClass(answer):
             return answer
 
 def answer():
-    answer = input("Enter your class: ").lower()
+    userinput = input("Enter your class: " + '\n').lower()
     loopCounter = 1
     for i in Class.allClasses:
         print(str(loopCounter) + ') ' + i.capitalize())
         loopCounter += 1
-    return answer
+    return userinput
 
 answer = answer()
 selectedClass = selectedClass(answer)
 print(selectedClass)
-while selectedClass == None:
-    answer = answer()
-    selectedClass = selectedClass()
-    print(selectedClass)
+
+def fight():
+	yourhp = YourStats.hp
+	enemyhp = EnemyStats.hp
+	round = 1
+	while yourhp > 0 and enemyhp > 0:
+		print('Round ' + str(round) + '\n')
+		print(str(yourhp) + '\n' + str(enemyhp) + '\n')
+		enemyhp -= YourStats.dmg
+		yourhp -= EnemyStats.dmg
+		print('your hp: ' + str(yourhp) + '\n')
+		print('enemy hp: ' + str(enemyhp) + '\n')
+		round += 1
+	if yourhp > enemyhp:
+		print('You won!!!')
+	else: 
+		print('Enemy Won :((')
+
+fight()
